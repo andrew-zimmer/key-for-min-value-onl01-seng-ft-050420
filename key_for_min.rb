78d4bc36f265a0445dce283fb439fe4e6ba5b5e7
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-07 20:21:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,9 @@ def key_for_min_value(name_hash)
   old_value = ""
 
   name_hash.collect do |name, value|
-    if old_key == "" 
+    if value == ""
+      return nil
+    elsif old_key == "" 
       old_key = name
       old_value = value 
     
